636cd3d510b2b8d7a7f761cce05267ae993a48fe
Omer Bowman <omer.bowman24@gmail.com>
2026-04-18 11:24:52 -0400

    Sample MTB movement

--- sample_sim.py
+++ sample_sim.py
@@ -3,6 +3,8 @@ Sample Python script to run a simulation in CoppeliaSim
 To be used as a starting point for the project
 '''
 
+import numpy as np
+import math
 import time
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
@@ -12,15 +14,35 @@ client = RemoteAPIClient('localhost', 23000)
 sim = client.require('sim')
 print('Connected')
 
+# Assign MTB joints
+q1 = sim.getObject('/MTB/axis')
+q2 = sim.getObject('/MTB/axis/link/axis')
+q3 = sim.getObject('/MTB/axis/link/axis/link/axis')
+q4 = sim.getObject('/MTB/axis/link/axis/link/axis/axis')
+print('Joints assigned')
+
+
 # Start the simulation
 print('Starting simulation...')
+sim.setStepping(True)
 sim.startSimulation()
 
-time.sleep(2)  # Placeholder
+try:
+    u1 = math.radians(30)
+    u2 = math.radians(-20)
+    u3 = 0.10
 
-sim_time = sim.getSimulationTime()
-print('Simulation time:', sim_time)
+    sim.setJointTargetPosition(q1, u1)
+    sim.setJointTargetPosition(q2, u2)
+    sim.setJointTargetPosition(q3, u3)
 
-# Stop the simulation
-sim.stopSimulation()
-print('Stopped')
+    for _ in range(100):
+        sim.step()
+
+finally:
+    sim_time = sim.getSimulationTime()
+    print('Simulation time:', sim_time)
+
+    # Stop the simulation
+    sim.stopSimulation()
+    print('Stopped')
